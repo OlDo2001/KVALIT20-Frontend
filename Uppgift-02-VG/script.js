b17a412ddfcf848dc38653a4c69bd13bdfb726d4
Olga Domorod <olga.domorod@gmail.com>
2020-12-14 21:39:48 +0100

    Kommentarer

--- Uppgift-02-VG/script.js
+++ Uppgift-02-VG/script.js
@@ -1,15 +1,31 @@
-function insert(num) {
-    document.querySelector('.output').value = document.querySelector('.output').value+num;
+/**
+ * Project: Uppgift-02-VG
+ * File: script.js 
+ * Klass: KVALIT20
+ * Kurs: Frontend
+ * Datum: 2020-12-11
+ * Förtfatare: Olga Domorod
+ */
+
+/**   En funktion som visar ett värde på varje knapp när du trycker på den.
+ *  En efter en (querySelector) på skärmen. */
+
+function insert(num) {  
+    document.querySelector('.display').value = document.querySelector('.display').value+num;
 }
-  
+
+ /**   En funktion som ersätter uttrycket på skärmen med dess resultat. */ 
+
 function equal() {
-    let expresion = document.querySelector('.output').value;
-  
+    let expresion = document.querySelector('.display').value;
+ /**   eval - en funktion som utvärderar och utför ett uttryck. */ 
     if(expresion) {
-      document.querySelector('.output').value = eval(document.querySelector('.output').value);
+      document.querySelector('.display').value = eval(document.querySelector('.display').value);
     }
 }
-  
+
+ /**   En funktion som rensar skärmens innehåll */   
+ 
 function clean() {
-    document.querySelector('.output').value="";
+    document.querySelector('.display').value="";
 }
